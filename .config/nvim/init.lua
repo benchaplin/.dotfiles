d89b08b9ed37801d0e9d3c47f3ef716db8585681
Ben Chaplin <benchaplin@protonmail.ch>
2022-08-29 17:05:31 -0400

    Reverse links dummy

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,3 @@
-/Users/benchaplin/.config/nvim/init.lua
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
---
+++ .config/nvim/lua/user/plugins.lua
@@ -0,0 +1,8 @@
+local Plug = vim.fn["plug#"]
+
+vim.call("plug#begin")
+
+Plug("neoclide/coc.nvim", { branch = "release" })
+
+vim.call("plug#end")
+
